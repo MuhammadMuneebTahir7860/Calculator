1026068b07db6b9754c3e674f5714c79f6366cd5
MuhammadMuneebTahir7860 <muhammadmuneebtahir7860@gmail.com>
2021-04-07 14:36:18 +0500

    Full Functional Calculator

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ function zero(){
     var firstZero=document.getElementById("output").innerHTML="0";
 }
 zero();
-function input(num1,opr,num2){
+function input(num){
     removeZero();
-    var output=document.getElementById("output").innerHTML;
-    if(output==0 || output=="+" || output=="-" || output=="*" || output=="/" || output=="%"){
-        document.getElementById("output").innerHTML=num1,opr,num2;
+
+    var output=document.getElementById("output");
+    if(output.innerHTML==0 || output.innerHTML=="+"  || output.innerHTML=="*" || output.innerHTML=="/" || output.innerHTML=="%"){
+        document.getElementById("output").innerHTML=num;
     }
-    else{
-        document.getElementById("output").innerHTML=output + num1,opr,num2;
+    else{    
+               if(!isNaN(num)){
+                   output.innerHTML+=num;
+               }
+               if(isNaN(num)){
+                   output.innerHTML+=num;
+                   if(isNaN(num)    &&  isNaN(output.innerHTML.charAt(output.innerHTML.length-2))){
+                       output.innerHTML=output.innerHTML.substring(0,output.innerHTML.length-2) +
+                       output.innerHTML.substring(output.innerHTML.length-1,output.innerHTML.length)
+                   }
+               }
+
     }
    
 }
@@ -25,7 +36,13 @@ function removeZero() {
     }
 }
 function calculate() {
-    var result = document.getElementById("output");
-    result.innerHTML = eval(result.innerHTML);
+    var result = document.getElementById("output").innerHTML;  
+    var second=document.getElementById("second")
+    if(isNaN(result.charAt(result.length-1))){
+        var lastCharNAN=result.substring(0,result.length-1);
+        second.innerHTML=eval(lastCharNAN);
+    }
+    else{
+        second.innerHTML=eval(result);
+    }
 }
-
